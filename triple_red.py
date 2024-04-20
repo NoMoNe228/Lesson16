e41bcd195147c3c2716b89c3e199ac633ca25f6b
Nomone288 <h1rana@yandex.ru>
2024-04-20 14:47:55 +0300

    ff

---
+++ triple_red.py
@@ -0,0 +1,17 @@
+import random
+
+# numbers = []
+# for i in range(100):
+#     numbers.append(random.randint(0, 100))
+# print(numbers)
+
+auto = []
+# for i in range(100):
+#     auto.append(input())
+#     if auto == 'tesla' or 'Jipp' or 'Shevrole':
+#         print('эти машины можно купить в россии')
+#     else:
+#         print('они находятся под санкциями')
+#auto.append(input() if 'tesla', 'jipp', 'shevrole' print('эти машины можно купить в россии'), else ('эти машины находятся под санкциями'))
+
+
